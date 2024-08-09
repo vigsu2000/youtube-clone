0d8fb8d4e9b1ec4738161ba34c6bcf0317cd5c08
Vignesh Suresh <vsuresh4@illinois.edu>
2024-08-09 14:00:56 -0400

    Add Dockerfile and deploy web app

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
-export default function Watch() {
+function WatchContent() {
   const videoPrefix = 'https://storage.googleapis.com/vigsu2000-processed-videos/';
   const videoSrc = useSearchParams().get('v');
 
   return (
     <div>
       <h1>Watch Page</h1>
-      { <video controls src={videoPrefix + videoSrc}/> }
+      { videoSrc && <video controls src={videoPrefix + videoSrc} /> }
     </div>
+  );
+}
+
+export default function Watch() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WatchContent />
+    </Suspense>
   );
 }
